3c4f459f36d26afa22ac072557abfdec63e984a9
Adam Amer <66858114+goldGoat@users.noreply.github.com>
2020-09-08 18:55:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+// You can use this variable in the for loop instead
+// var numberOfDrumButtons= document.querySelectorAll(".drum").length;
+
+
+// Detecting Button Press
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+
+  });
+}
+
+// Detecting keyboard press
+
+document.addEventListener("keydown", function(event){
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+// Switching on the key parameter
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default: console.log();
+
+  }
+}
+
+function buttonAnimation(currentKey){
+
+  var activeButton= document.querySelector("." + currentKey)
+
+activeButton.classList.add("pressed");
+
+setTimeout(function () {
+  activeButton.classList.remove("pressed");
+}, 100);
+
+}
+
+
+
+
+// Another way of doing this
+
+// //Click Event Listener
+// for (i=0; i<7; i++)
+//     document.querySelectorAll(".drum")[i].addEventListener("click",playSound);
+//
+// //Key Press Event Listener
+// document.addEventListener("keydown",playSound);
+//
+// //Funtion to play sounds
+// function playSound(event){
+//     var keyVal;
+//     if (event.type=="click"){
+//         keyVal = this.innerHTML;
+//     }
+//     if (event.type=="keydown"){
+//         keyVal = event.key;
+//     }
+//     switch (keyVal) {
+//
+//         //cases
+//
+//     }
+// }
